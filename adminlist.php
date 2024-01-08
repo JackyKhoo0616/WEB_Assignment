ea54499eab8f3817cd424a6b56d4844979fc0851
uwuwu33 <cirnomatonline@gmail.com>
2024-01-08 22:30:44 +0800

    adminlist + editpage

--- adminlist.php
+++ adminlist.php
@@ -1,9 +1,9 @@
 <?php
 include 'Connection.php';
 $query = "SELECT * FROM breezequiz";
+
 $result = mysqli_query($connection, $query);
 ?>
-
 <table width=100% border=1>
     <tr>
         <th>ID</th>
@@ -21,9 +21,8 @@ while ($row = mysqli_fetch_assoc($result)){
         <td><?php echo $row["Last Name"]; ?> </td>
         <td><?php echo $row["Email"]; ?> </td>
         <td><?php echo $row["Country"]; ?> </td>
-        <td><a href="editPage.php?id=<?php echo $row['id']; ?>">Edit</a></td>
-
-        <td><a href="deletePage.php?id=<?php echo $row['id']; ?>">Delete</a></td>
+        <td><a href="editpage.php?id=<?php echo $row['ID']; ?>">Edit</a></td>
+        <td><a href="deletePage.php?id=<?php echo $row['ID']; ?>">Delete</a></td>
     </tr>
 <?php
 }
