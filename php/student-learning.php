8dc1d0ad71e99ff122d525416f3fa2d32ed07db7
uwuwu33 <cirnomatonline@gmail.com>
2024-01-18 12:59:23 +0800

    viewlearning and viewquiz

---
+++ php/student-learning.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Learning Material</title>
+
+		<link rel="stylesheet" href="../css/student-learning.css" />
+		<link rel="stylesheet" href="../css/footer.css" />
+	</head>
+	<body>
+		<div class="learning-content">
+			<h1>Learning Material 1</h1>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+				Aspernatur, quaerat numquam porro, accusamus minus repudiandae
+				tenetur sint error officiis inventore rerum excepturi natus enim
+				reiciendis iste dolor incidunt neque? Vitae tempore debitis ab
+				ea ipsum aliquam, excepturi neque omnis saepe, atque, accusamus
+				eveniet alias blanditiis. Voluptas delectus consequatur in
+				quaerat omnis vel nostrum! Iste eos id cumque, ad commodi cum
+				rerum dolor. Odio, quia! Officiis molestias odio, eligendi
+				dolorum maxime provident quia aspernatur delectus dolorem
+				corporis ipsum laboriosam labore architecto dolore accusantium
+				alias adipisci minus quaerat tempora quasi natus rem veritatis
+				pariatur qui! Ducimus optio fuga quae eos magnam voluptatibus
+				eius aliquid vel consequatur. Architecto magnam eum dicta
+				reiciendis obcaecati, debitis eaque numquam, at repellendus ad
+				velit nulla aspernatur. Dolores, inventore adipisci. Eaque nihil
+				accusamus reprehenderit cum molestiae eum ullam alias minima
+				neque, incidunt corrupti distinctio architecto aliquam sed,
+				expedita beatae nam repudiandae soluta similique. Quisquam
+				doloribus earum blanditiis sed voluptatum alias sapiente maxime
+				optio dolorem natus! Natus labore quidem praesentium excepturi
+				alias sapiente eos. Quisquam, tempora possimus? Quod, non!
+				Corporis, suscipit distinctio quos modi eaque adipisci animi
+				facere ea vero possimus ut at aut ipsa commodi! Reprehenderit,
+				odit similique quibusdam exercitationem consectetur dicta
+				obcaecati beatae commodi architecto dolorum soluta!
+			</p>
+
+			<div class="additional">
+				<iframe
+					width="1200"
+					height="600"
+					src="https://www.youtube.com/embed/JD5szTvK8yo?si=cPm5i_mylc-yWIRe"
+					title="YouTube video player"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					allowfullscreen
+				></iframe>
+			</div>
+		</div>
+
+		<!-- footer -->
+		<div class="footer">
+			<div class="left">
+				<div class="desc">
+					<h2>BreezeQuiz</h2>
+					<p>
+						BreezeQuiz is an interactive quiz platform enhancing
+						learning with gamification, real-time analytics, and a
+						resource library for students and educators to improve
+						engagement and performance.
+					</p>
+				</div>
+			</div>
+			<div class="right">
+				<div class="footer-logo">
+					<a href="../html/user-index.html" target="_blank">
+						<img src="../picture/logo.png" />
+					</a>
+				</div>
+				<div class="contact">
+					<p>
+						<span class="contact-span1">Email:</span>
+						breezequiz@gmail.com
+					</p>
+					<p>
+						<span class="contact-span2">Phone:</span> 09 - 523 -
+						2288
+					</p>
+				</div>
+				<div class="social_media">
+					<div class="social_media_icons">
+						<div class="facebook">
+							<a href="https://www.facebook.com/">
+								<img src="../picture/facebook.svg" />
+							</a>
+						</div>
+						<div class="instagram">
+							<a href="https://www.instagram.com/">
+								<img src="../picture/instagram.svg" />
+							</a>
+						</div>
+						<div class="twitter">
+							<a href="https://twitter.com/home">
+								<img src="../picture/twitter.svg" />
+							</a>
+						</div>
+						<div class="github">
+							<a href="https://github.com/">
+								<img src="../picture/github.svg" />
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- copyright part -->
+		<div class="copyright">
+			<p>© 2024 BreezeQuiz. All rights reserved.</p>
+		</div>
+	</body>
+</html>
